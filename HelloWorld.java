679f9472c88e41dffe343ba11c4603adc88a263d
carlos <cmorata@gmail.com>
2024-04-25 11:05:44 +0200

    mes canvis

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,9 +5,17 @@ class HelloWorld {
 
     public static void main(String[] args) {
 		while(true){
+
+
+--------------------main ghiurehgiureoir
 			System.out.println("Hello, World! vol dir Hola MON!!!! + Nova funcionalitat");
 			System.out.println("canvi gros");
 		}
+
+--------------------nuevab ghiurehgiureoir
+			System.out.println("Hello, World! vol dir Hola MON!!!! + Nova funcionalitat");
+			System.out.println("canvi gros");
 		
     }
+
 }
