98276b9e1b448ce7bc2e8ee0d79923e434750cfe
carlos <cmorata@gmail.com>
2024-04-25 10:55:58 +0200

    fi nova F

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ class HelloWorld {
 
     public static void main(String[] args) {
 		while(true){
-			System.out.println("Hello, World! vol dir Hola MON!!!!");
+			System.out.println("Hello, World! vol dir Hola MON!!!! + Nova funcionalitat");
 		}
 		
     }
