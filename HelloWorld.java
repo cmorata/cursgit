53ca3869c9df67b352f944727abca73e86895c53
carlos <cmorata@gmail.com>
2024-04-25 10:59:14 +0200

    canvis

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,7 @@ class HelloWorld {
     public static void main(String[] args) {
 		while(true){
 			System.out.println("Hello, World! vol dir Hola MON!!!! + Nova funcionalitat");
+			System.out.println("On estem + Nova funcionalitat");
 		}
 		
     }
